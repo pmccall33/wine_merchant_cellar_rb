0489a60ea76da2be2d3ae25c40305c2acdaf40ea
pmccall <pmccall33@gmail.com>
2019-02-24 19:59:09 -0600

    create route working

--- controllers/WineController.rb
+++ controllers/WineController.rb
@@ -13,8 +13,35 @@ class WineController < ApplicationController
 		erb :wine_index
 	end
 
+	# new route
+	get '/new' do
+		erb :wine_new
+	end
 
+	# create/ POST route
+	post '/' do
+		puts "You have hit the POST route, congrats"
+		wine = Wine.new
 
+		wine.name = params[:name]
+		wine.winemaker = params[:winemaker]
+		wine.region = params[:region]
+		wine.style  = params[:style]
+		wine.year = params[:year]
 
+# 		CREATE TABLE wines(
+		#   id SERIAL PRIMARY KEY,
+		#   name VARCHAR(255),
+		#   winemaker VARCHAR(255),
+		#   region VARCHAR(255),
+		#   style VARCHAR(255),
+		#   year SMALLINT
+		# );
+
+    	wine.save
+
+    	redirect '/wines'
+
+	end
 
 end
